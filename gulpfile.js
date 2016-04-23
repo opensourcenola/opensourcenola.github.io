9a88d3f4390de9339843de0f40ed85c72eb3f6a9
Bryan Joseph <bryanjos@gmail.com>
2016-04-23 12:27:27 -0500

    Add sponsor images

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
 var browserSync = require('browser-sync');
-var run = require('gulp-run');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var wrap = require('gulp-wrap');
@@ -24,11 +23,9 @@ gulp.task('build-sass', function() {
       .pipe(gulp.dest('dest/css'));
 });
 
-gulp.task('build-js', function() {
-  run('jspm bundle-sfx src/js/main dest/js/app.js').exec();
+gulp.task('build', ['build-sass', 'build-html'], function(){
+  browserSync.reload();
 });
-
-gulp.task('build', ['build-sass', 'build-html']);
 
 gulp.task('serve', ['build'], function(done) {
   browserSync({
@@ -45,7 +42,7 @@ gulp.task('serve', ['build'], function(done) {
 });
 
 gulp.task('watch', ['serve'], function() {
-  gulp.watch(['src/html/**/*.html','src/js/**/*.js', 'src/css/**/*.scss'], ['build', browserSync.reload]).on('change', reportChange);
+  gulp.watch(['src/html/**/*.html','src/js/**/*.js', 'src/css/**/*.scss'], ['build']).on('change', reportChange);
 });
 
 gulp.task('default', ['build']);
